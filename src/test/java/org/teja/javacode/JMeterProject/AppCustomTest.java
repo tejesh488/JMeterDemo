83105f5f8874e21db902fe771fa221b4dd1be896
Kumar Padarthi <t.kumar.padarthi@accenture.com>
2018-06-12 15:01:57 +0530

    changes

---
+++ src/test/java/org/teja/javacode/JMeterProject/AppCustomTest.java
@@ -0,0 +1,15 @@
+package org.teja.javacode.JMeterProject;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class AppCustomTest {
+
+	
+	@Test
+	public void simpleTest()
+	{
+		Assert.assertEquals(true, true);
+	}
+}
